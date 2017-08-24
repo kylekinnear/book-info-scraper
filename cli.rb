06f59001bd1b61ecffe8be0e26db37fb0338076f
kylek <kbelway29@gmail.com>
2017-08-24 15:49:35 -0400

    reordered output

--- cli.rb
+++ cli.rb
@@ -8,8 +8,8 @@ puts "Welcome to Book Info Scraper!"
 book = Scraper.new
 book.scrape
 
-puts "Author:".ljust(22) << "#{book.author}"
 puts "Title:".ljust(22) << "#{book.title}"
+puts "Author:".ljust(22) << "#{book.author}"
 puts "Series:".ljust(22) << "#{book.series}"
 if book.release_date.size > 0
   puts "Release Date:".ljust(22) << "#{book.release_date}"

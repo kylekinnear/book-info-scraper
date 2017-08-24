9f910a39e9445b81daafdda24362bee0ab8ce9ae
kylek <kbelway29@gmail.com>
2017-08-24 16:13:46 -0400

    added error message for nobook

--- scraper.rb
+++ scraper.rb
@@ -11,11 +11,21 @@ class Scraper
       puts "Checking now!"
       input.gsub!(/(\W)+/, "+") #formats input as valid search string
       search_page = Nokogiri::HTML(open("https://www.goodreads.com/search?q=#{input}&search_type=books")) #interpolates input to search url and pulls page with nokogiri
-      item_page = Nokogiri::HTML(open("https://goodreads.com/#{search_page.css("table a").attribute("href").value}"))
+      if search_page.css("table a").size != 0
+        item_page = Nokogiri::HTML(open("https://goodreads.com/#{search_page.css("table a").attribute("href").value}"))
+      else
+        raise NoBook
+      end
       @author = item_page.css("div#bookAuthors.stacked span :not(.greyText) :not(.smallText)").text
       @title = item_page.css("h1#bookTitle.bookTitle").text.reverse.strip.reverse.lines.first.chomp #provides title
       @series = ((item_page.css("h1#bookTitle.bookTitle :first-child").text.strip).sub "(", "").sub ")", "" #provides series
       @release_date = item_page.css("div#details .row").text.split(/\n+/)[2].sub /\A\s+/, "" #provides the release date
+    end
+  end
+
+  class NoBook < StandardError
+    def message
+      "A Goodreads page for this book does not yet exist. Check back in a while!"
     end
   end
 
